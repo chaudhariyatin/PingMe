3082e212ff52d7ea6ac307663c3f99fd710339d8
chaudhariyatin <yatinchaudhari983@gmail.com>
2023-01-18 00:04:42 +0530

    first commit

--- mobile-frontend/ios/Pods/Target Support Files/Pods-pingme-pingmeTests/Pods-pingme-pingmeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_pingme_pingmeTests : NSObject
-@end
-@implementation PodsDummy_Pods_pingme_pingmeTests
-@end
--- mobile-frontend/ios/Pods/Target Support Files/Pods-pingme/Pods-pingme-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_pingme : NSObject
-@end
-@implementation PodsDummy_Pods_pingme
-@end
